ddd48c63f4f76935e9d93aa8d7514ae9d6d362e1
datleXx <dat91fitzz@gmail.com>
2026-06-08 10:59:38 +0700

    feat: add batch method

--- src/pingest/pipeline.py
+++ src/pingest/pipeline.py
@@ -1,4 +1,20 @@
+import itertools
+
+
 def read_records(source):
     """Yield one record at a time. Source is consumed exactly once"""
     for rec in source:
         yield rec
+
+
+def batched(iterable, n):
+    """Yield a list of n at once"""
+    if n <= 0:
+        raise ValueError("n must be >= 0")
+    source = iter(iterable)
+    while True:
+        chunk = list(itertools.islice(source, n))
+        if chunk:
+            yield chunk
+        else:
+            break
