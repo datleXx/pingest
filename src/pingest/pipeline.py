39d8039c3ed98c4d5f0f7adc56bdc8a677f37473
datleXx <dat91fitzz@gmail.com>
2026-06-08 11:55:46 +0700

    feat: pipeline done + test

--- src/pingest/pipeline.py
+++ src/pingest/pipeline.py
@@ -1,4 +1,7 @@
+import tracemalloc
 import itertools
+import functools
+from typing import Callable, Dict, Iterable, List
 
 
 def read_records(source):
@@ -18,3 +21,58 @@ def batched(iterable, n):
             yield chunk
         else:
             break
+
+
+def filter_records(records: Iterable[Dict], predicate):
+    """Filter list of dicts when predicate on record is truthy"""
+    for record in records:
+        if predicate(record):
+            yield record
+
+
+def transform_records(records, func):
+    """Applies `func` to each record and yields the result"""
+    for record in records:
+        yield func(record)
+
+
+def run_pipeline(source, stages: List[Callable[[Iterable], Iterable]]):
+    if not stages:
+        return
+    current = source
+    for func in stages:
+        current = func(current)
+
+    yield from current
+
+
+def giant_source(n: int):
+    for i in range(n):
+        yield {"id": i, "value": i * 2}
+
+
+test_souce = giant_source(10_000_000)
+
+test_pipe_transform = functools.partial(
+    transform_records, func=lambda r: {**r, "square": r["value"] ** 2}
+)
+
+test_pipe_filter = functools.partial(
+    filter_records, predicate=lambda r: r["square"] % 2 == 0
+)
+
+pipe_func = [read_records, test_pipe_transform, test_pipe_filter]
+
+tracemalloc.start()
+
+pipeline = run_pipeline(test_souce, pipe_func)
+
+for i, _ in enumerate(pipeline):
+    if i % 1_000_000 == 0:
+        current, peak = tracemalloc.get_traced_memory()
+        print(
+            f"Processed {i:>8,} records  |  current: {current / 1024:>8.1f} KiB  |  peak: {peak / 1024:>8.1f} KiB"
+        )
+
+_, peak = tracemalloc.get_traced_memory()
+print(f"\nFinal peak: {peak / 1024 / 1024:.2f} MiB")
